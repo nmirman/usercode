ac82761cb7d8100d8791ba4bf5a723b004972beb
nmirman <>
2012-06-12 18:08:40 +0000

    Added Aleko's files.

---
+++ JetMETAnalysis/METSigAnalysis/interface/MtupleFormats.h
@@ -0,0 +1,141 @@
+//maximum number of particles and jets saved.
+const Int_t NMuons=1000;
+const Int_t NParticles = 50000;
+const Int_t NJets = 10000;  //for Wenu study it is more than enough
+const Int_t NMETs = 100;
+const Int_t NVertices = 100;
+
+struct METs{
+    Int_t size;
+    float et[NMETs];
+    float phi[NMETs];
+    float sig[NMETs];
+    float sumEt[NMETs];
+    float dxx[NMETs];
+    float dxy[NMETs];
+    float dyy[NMETs];
+};
+
+struct PFJets{
+    Int_t size;
+    Int_t nco[NJets];
+    float l1[NJets];
+    float l1l2l3[NJets];
+    float pt[NJets];
+    float phi[NJets];
+    float eta[NJets];
+    float dpt[NJets];
+    float dphi[NJets];
+};
+
+struct PFCandidates{
+    Int_t size;
+    Int_t type[NParticles];
+    float pt[NParticles];
+    float et[NParticles];
+    float phi[NParticles];
+    float eta[NParticles];
+    Int_t jetIndex[NParticles];
+};
+
+struct GenW{
+    int id;
+    int eid;
+    int nid;
+    double M;
+    double we;
+    double wpx;
+    double wpy;
+    double wpz;
+    double wpt;
+    double weta;
+    double wphi;
+    double wy;
+    double m;
+    double ee;
+    double epx;
+    double epy;
+    double epz;
+    double ept;
+    double eeta;
+    double ephi;
+    double ne;
+    double npx;
+    double npy;
+    double npz;
+    double npt;
+    double neta;
+    double nphi;
+};
+
+struct Vertices{
+    Int_t size;
+    Bool_t isFake[NVertices];
+    float ndof[NVertices];
+    float x[NVertices];
+    float y[NVertices];
+    float z[NVertices];
+    float Rho[NVertices];
+};
+
+
+struct RecoMuon{
+    int size;
+    int charge[NMuons];
+    int isGlobal[NMuons];
+    int isTracker[NMuons];
+    int trackerHits[NMuons];
+    int pixelHits[NMuons];
+    int muonHits[NMuons];
+    int nMatches[NMuons];
+    int numberOfValidHits[NMuons];
+    int numberOfValidPixelHits[NMuons];
+    int numberOfValidStripHits[NMuons];
+    int numberOfValidPixelBarrelHits[NMuons];
+    int pixelLayersWithMeasurement[NMuons];
+    int pixelBarrelLayersWithMeasurement[NMuons];
+    int pixelEndcapLayersWithMeasurement[NMuons];
+    int numberOfValidTrackerLayers[NMuons];
+    float chi2[NMuons];
+    float pt[NMuons];
+    float glpt[NMuons];
+    float trpt[NMuons];
+    float p[NMuons];
+    float e[NMuons];
+    float phi[NMuons];
+    float eta[NMuons];
+    float px[NMuons];
+    float py[NMuons];
+    float pz[NMuons];
+    float dxy[NMuons];
+    float dr03TkSumPt[NMuons];
+    float dr03EcalRecHitSumEt[NMuons];
+    float dr03HcalTowerSumEt[NMuons];
+    float drTO[20][NMuons];
+
+
+    float gdr[NMuons];
+    int   mid[NMuons];
+    float mpx[NMuons];
+    float mpy[NMuons];
+    float mpz[NMuons];
+    float men[NMuons];
+};
+
+
+struct GenInfo {
+    Int_t pid;
+    float pthat;
+    float alphaQCD;
+    float alphaQED;
+    float scalePDF;
+    Int_t id1;
+    Int_t id2;
+    float x1;
+    float x2;
+    float xPDF1;
+    float xPDF2;
+    float weight;
+    float xsec;
+    float eff;
+};
---
+++ JetMETAnalysis/METSigAnalysis/test/config.py
@@ -0,0 +1,153 @@
+ISMC = True
+
+import FWCore.ParameterSet.Config as cms
+process = cms.Process("NTUPLE")
+
+process.load("FWCore.MessageService.MessageLogger_cfi")
+process.MessageLogger.cerr.FwkReport.reportEvery = 1000
+
+process.source = cms.Source("PoolSource", 
+    fileNames = cms.untracked.vstring(
+	'file:../DY_44X_V9B.root',
+    )
+)
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
+
+process.load("Configuration.StandardSequences.Geometry_cff")
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+process.GlobalTag.globaltag = ('GR_R_44_V13::All','START44_V5D::All')[ISMC]
+process.load("JetMETCorrections.Configuration.DefaultJEC_cff")
+
+
+from PhysicsTools.SelectorUtils.pvSelector_cfi import pvSelector
+process.goodOfflinePrimaryVertices = cms.EDFilter(
+    "PrimaryVertexObjectFilter",
+    filterParams = pvSelector.clone( minNdof = cms.double(4.0), maxZ = cms.double(24.0) ),
+    src=cms.InputTag('offlinePrimaryVertices')
+)
+
+process.load("CommonTools.ParticleFlow.PF2PAT_cff")
+
+process.pfAllMuons.src="particleFlow"
+process.pfMuonsFromVertex.dzCut=9999
+process.pfSelectedMuons.cut = cms.string('pt>15 && abs(eta)<2.4')
+process.pfNoMuon.bottomCollection		 = "particleFlow"
+process.pfNoMuon.topCollection			 = "pfSelectedMuons"
+process.pfJets.doAreaFastjet			 = True
+process.pfJets.jetPtMin				 = 0
+process.pfJets.src				 = "pfNoMuon"
+
+process.load("RecoJets.JetProducers.ak5PFJets_cfi")
+process.ak5PFJets.doAreaFastjet = cms.bool(True)
+
+from RecoJets.JetProducers.kt4PFJets_cfi import kt4PFJets
+process.kt6PFJets = kt4PFJets.clone(
+    src = cms.InputTag('pfNoMuon'),
+    rParam = cms.double(0.6),
+    doRhoFastjet = cms.bool(True),
+    Rho_EtaMax = cms.double(2.5)
+)
+
+process.mypf2pat = cms.Sequence(process.pfAllMuons * 
+				process.pfMuonsFromVertex*
+				process.pfSelectedMuons*
+				process.pfNoMuon*
+				process.kt6PFJets * 
+				process.pfJets*
+				process.pfMET)
+
+####MET modules
+process.load("JetMETAnalysis.METSignificance.metsignificance_cfi")
+process.pfMetSig.src = cms.VInputTag("pfJets", "particleFlow") 
+#these should be reproducible from the ntuples...
+process.pfMetSigSmeared    =  process.pfMetSig.clone(scaleResolutions=True) 
+process.pfMetSigSmeared15  = (process.pfMetSigSmeared.clone(smearJetPtThreshold=15), process.pfMetSigSmeared.clone(smearJetPtThreshold=15, smearMet=True))[ISMC]
+
+metList = []
+metList.append(cms.untracked.InputTag("pfMet", "", ""))
+metList.append(cms.untracked.InputTag("pfMET", "", ""))
+metList.append(cms.untracked.InputTag("pfMetSig", "", "NTUPLE")) #this is the central one
+metList.append(cms.untracked.InputTag("pfMetSigSmeared15", "","NTUPLE"))
+
+process.mymets = cms.Sequence(process.pfMetSig * process.pfMetSigSmeared15)
+
+trigger_paths = ["HLT_IsoMu15_v",
+		 "HLT_IsoMu24_v",
+		 "HLT_Mu24_v"
+]
+trigger_pattern = [path+"*" for path in trigger_paths]
+
+
+process.ntuple = cms.EDAnalyzer('Mtuple',
+	debug		    = cms.untracked.bool(False),
+	isMC		    = cms.untracked.bool(ISMC),
+	OutputFileName	    = cms.untracked.string('ntuple1.root'),
+
+	saveTriggerResults  = cms.untracked.bool(True),
+	TriggerResultsTag   = cms.untracked.InputTag("TriggerResults","","HLT"),
+	TriggerEventTag	    = cms.untracked.InputTag("hltTriggerSummaryAOD","","HLT"),
+	TriggerPath	    = cms.untracked.vstring(trigger_paths),
+
+	saveMuons	    = cms.untracked.bool(True),
+	muonTag		    = cms.untracked.InputTag("muons"),
+
+	saveParticles	    = cms.untracked.bool(True),
+	genparticlesTag	    = cms.untracked.InputTag("genParticles"),
+	pfcandidatesTag     = cms.untracked.InputTag("particleFlow"),
+	
+	saveJets	    = cms.untracked.int32(10000),
+	pfjetsTag	    = cms.untracked.InputTag("pfJets"),
+	pfjetCorrector	    = cms.untracked.string("ak5PFL1Fastjet"),
+	pfjetCorrector2	    = cms.untracked.string("ak5PFL1FastL2L3"),
+	jetResolAlgo	    = cms.string('AK5PF'),
+	jetResolEra	    = cms.string('Spring10'),
+
+	saveMETs	    = cms.untracked.bool(True),
+	mets		    = cms.untracked.VInputTag(metList),
+	genMet		    = cms.untracked.InputTag("myGenMetCalo"),
+
+    	saveVertices	    = cms.untracked.bool(True),
+	verticesTag	    = cms.untracked.InputTag("offlinePrimaryVertices"),
+
+	pileupTag	    = cms.untracked.InputTag("addPileupInfo"),
+)
+if not ISMC:
+    process.ntuple.pfjetCorrector2 = "ak5PFL1FastL2L3Residual"
+
+
+for i in xrange(len(metList)): print "%s: %s" %(i,metList[i])
+
+#trigger filter			       
+from HLTrigger.HLTfilters.hltHighLevel_cfi import *
+process.triggerSelection = hltHighLevel.clone(TriggerResultsTag = "TriggerResults::HLT", HLTPaths = trigger_pattern, throw=False)
+
+#I will only this for the data
+process.noscraping = cms.EDFilter("FilterOutScraping",
+                                applyfilter = cms.untracked.bool(True),
+                                debugOn = cms.untracked.bool(False),
+                                numtrack = cms.untracked.uint32(10),
+                                thresh = cms.untracked.double(0.25)
+                                )
+
+
+process.load("RecoMET.Configuration.GenMETParticles_cff")
+process.load("RecoMET.METProducers.genMetCalo_cfi")
+process.myGenMetCalo = process.genMetCalo.clone(src = "genParticlesForMETAllVisible")
+
+
+process.p = cms.Path( process.noscraping *
+		      process.triggerSelection * 
+		      process.mypf2pat *
+		      process.mymets *
+		      process.genParticlesForMETAllVisible*
+		      process.myGenMetCalo*
+		      process.ntuple
+		      )
+
+if ISMC:
+    process.p.remove(process.noscraping)
+    process.p.remove(process.triggerSelection)
+else:
+    process.p.remove(process.genParticlesForMETAllVisible)
+    process.p.remove(process.myGenMetCalo)
