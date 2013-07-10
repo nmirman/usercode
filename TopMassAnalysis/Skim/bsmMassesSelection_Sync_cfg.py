bf7ace78a073e4114be35254f2baa04085085f2c
nmirman <>
2013-07-10 15:26:23 +0000

    Updated skim configuration to synchronize with Top Reference Selection 2012.

--- TopMassAnalysis/Skim/bsmMassesSelection_Sync_cfg.py
+++ TopMassAnalysis/Skim/bsmMassesSelection_Sync_cfg.py
@@ -12,19 +12,31 @@ options.setDefault( 'outputFile',
                   "BsmMassesSkim_Summer11_Sync.root" )
                   
 options.setDefault( 'inputFiles',
-                    '/store/data/Run2012D/DoubleMu/AOD/16Jan2013-v2/10000/00A4899E-666B-E211-A2AC-E0CB4E29C50D.root')
+                    #'/store/data/Run2012D/DoubleMu/AOD/16Jan2013-v2/10000/00A4899E-666B-E211-A2AC-E0CB4E29C50D.root')
+                    #'/store/mc/Summer12_DR53X/TTJets_MassiveBinDECAY_TuneZ2star_8TeV-madgraph-tauola/AODSIM/PU_S10_START53_V7A-v1/0000/FE4C2F81-D0E1-E111-9080-0030487E0A2D.root'
+                    '/store/caf/user/tjkim/mc/Summer12_DR53X/TTJets_FullLeptMGDecays_8TeV-madgraph/AODSIM/PU_S10_START53_V7A-v2/00000/000C5D15-AB1A-E211-8BDE-00215E22053A.root',
+                    '/store/caf/user/tjkim/mc/Summer12_DR53X/TTJets_FullLeptMGDecays_8TeV-madgraph/AODSIM/PU_S10_START53_V7A-v2/00000/0010005A-421A-E211-9E3C-E41F13181DA4.root'
+                    )
 
 options.register( 'inputType',
                   'data',
                   VarParsing.multiplicity.singleton,
                   VarParsing.varType.string,
-                  'Type of input file. Options are data, signalMC, tauMC, topNonDilMC, backgroundMC.')
+                  'Type of input file. Options are data, signalMC, tauMC, topNonDilMC, backgroundMC.'
+                  )
 
 options.register( 'globalTag',
                   'START53_V7A',
                   VarParsing.multiplicity.singleton,
                   VarParsing.varType.string,
                   'globalTag')
+
+options.register( 'dataType',
+                  'DoubleMuData',
+                  VarParsing.multiplicity.singleton,
+                  VarParsing.varType.string,
+                  'Type of data file.'
+                  )
 
 options.register( 'wantSummary',
                   False,
@@ -36,33 +48,44 @@ options.parseArguments()
 
 process = cms.Process('PAT')
 
-if (options.inputType == 'signalMC') :
-    runOnMC = True
-    signalMC = True
-    TauMC = False
-    TopNonDilMC = False
-elif (options.inputType == 'tauMC') :
-    runOnMC = True
-    signalMC = False
-    TauMC = True
-    TopNonDilMC = False
-elif (options.inputType == 'topNonDilMC') :
-    runOnMC = True
-    signalMC = False
-    TauMC = False
-    TopNonDilMC = True
-elif (options.inputType == 'backgroundMC') :
-    runOnMC = True
-    signalMC = False
-    TauMC = False
-    TopNonDilMC = False
-elif (options.inputType == 'data') :
-    runOnMC = False
-    signalMC = False
-    TauMC = False
-    TopNonDilMC = False
-else :
-    sys.exit("inputType value was invalid. Options are data, signalMC, tauMC, topNonDilMC, backgroundMC.")
+if ( 'MC' in options.dataType ) :
+   runOnMC = True
+   trigger_paths = ["HLT_Mu17_Mu8_v17","HLT_Mu17_TkMu8_v10","HLT_Ele17_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_Ele8_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_v17","HLT_Mu17_Ele8_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_v7","HLT_Mu8_Ele17_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_v7"]
+   options.globalTag = 'START53_V7A'
+   if ( 'TTJets' in options.dataType ) :
+      signalMC = True
+      TauMC = False
+      TopNonDilMC = False
+   elif ( 'T_tW' in options.dataType ) :
+      signalMC = False
+      TauMC = False
+      TopNonDilMC = True
+   elif ( 'WJetsToLNu' in options.dataType ) :
+      signalMC = False
+      TauMC = False
+      TopNonDilMC = False
+   elif ( 'DYJetsToLL' in options.dataType ) :
+      signalMC = False
+      TauMC = False
+      TopNonDilMC = False
+   else :
+      sys.exit('dataType value was invalid.  The MC type does not exist.')
+
+elif ( 'Data' in options.dataType ) :
+   runOnMC = False
+   signalMC = False
+   TauMC = False
+   TopNonDilMC = False
+   options.globalTag = 'FT_53_V21_AN4'
+   if ( 'DoubleMu' in options.dataType ) :
+      trigger_paths = ["HLT_Mu17_Mu8_v","HLT_Mu17_TkMu8_v"]
+   elif ( 'DoubleElectron' in options.dataType ) :
+      trigger_paths = ["HLT_Ele17_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_Ele8_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_v"]
+   elif ( 'MuEG' in options.dataType ) :
+      trigger_paths = ["HLT_Mu17_Ele8_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_v","HLT_Mu8_Ele17_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_v"]
+   else : 
+      sys.exit('dataType value was invalid.  The Data type does not exist.')
+
 
 postfix = 'PFlow'
 
@@ -73,16 +96,6 @@ globalTag = options.globalTag
 fwkReportEvery = 1000
 
 outCommands = ['drop *']
-
-muonCutPF = "isGlobalMuon & isTrackerMuon & pt>20 & abs(eta)<2.4 & numberOfValidHits>10. & muonID('GlobalMuonPromptTight')"
-electronCutPF = "pt>20 & abs(eta)<2.5 & test_bit( electronID('eidTightMC'), 0)"
-jetCutPF = 'pt>30 & abs(eta)<2.5'
-jetCutPF += ' && numberOfDaughters > 1'                                  # PF jet ID:
-jetCutPF += ' && chargedEmEnergyFraction < 0.99'                         # PF jet ID:
-jetCutPF += ' && neutralHadronEnergyFraction < 0.99'                     # PF jet ID:
-jetCutPF += ' && neutralEmEnergyFraction < 0.99'                         # PF jet ID:
-jetCutPF += ' && (chargedHadronEnergyFraction > 0. || abs(eta) >= 2.4)'  # PF jet ID:
-jetCutPF += ' && (chargedMultiplicity > 0 || abs(eta) >= 2.4)'           # PF jet ID:
 
 
 ####################################################
@@ -168,9 +181,6 @@ elif TopNonDilMC:
     process.ttDilFilter.restrictTauDecays.leptonic = cms.bool(True)
     process.ttDilFilter.invert = True
 
-# Gen Kinematics Filter for events with Pythia bug
-process.load("GeneratorInterface.GenFilters.TotalKinematicsFilter_cfi")
-
 # event cleaning
 from CommonTools.RecoAlgos.HBHENoiseFilter_cfi import *
 process.HBHENoiseFilter = HBHENoiseFilter
@@ -246,50 +256,87 @@ applyPostfix( process, 'pfNoTau'     , postfix ).enable = False
 applyPostfix( process, 'pfPileUp', postfix ).Vertices = cms.InputTag( 'goodOfflinePrimaryVertices' )
 applyPostfix( process, 'pfPileUp', postfix ).checkClosestZVertex = False
 
+# object configurations from
+# https://twiki.cern.ch/twiki/bin/view/CMS/TwikiTopRefHermeticTopProjections
+
 # jet configuration
+jetCutPF = 'pt>30 & abs(eta)<2.5'
+jetCutPF += ' && numberOfDaughters > 1'                                  # PF jet ID:
+jetCutPF += ' && chargedEmEnergyFraction < 0.99'                         # PF jet ID:
+jetCutPF += ' && neutralHadronEnergyFraction < 0.99'                     # PF jet ID:
+jetCutPF += ' && neutralEmEnergyFraction < 0.99'                         # PF jet ID:
+jetCutPF += ' && (chargedHadronEnergyFraction > 0. || abs(eta) >= 2.4)'  # PF jet ID:
+jetCutPF += ' && (chargedMultiplicity > 0 || abs(eta) >= 2.4)'           # PF jet ID:
+
 applyPostfix( process, 'pfJets', postfix ).doAreaFastjet = True
 applyPostfix( process, 'pfJets', postfix ).doRhoFastjet  = False
 applyPostfix( process, 'selectedPatJets', postfix ).cut  = jetCutPF
 
 
-
 # muon configuration
-applyPostfix( process, 'pfMuonsFromVertex'    , postfix ).vertices = cms.InputTag( 'goodOfflinePrimaryVertices' )
-applyPostfix( process, 'pfMuonsFromVertex'    , postfix ).d0Cut    = 0.02 # this is wrt the primary vertex
-#applyPostfix( process, 'isoValMuonWithCharged', postfix ).deposits[0].deltaR = 0.3
-#applyPostfix( process, 'isoValMuonWithNeutral', postfix ).deposits[0].deltaR = 0.3
-#applyPostfix( process, 'isoValMuonWithPhotons', postfix ).deposits[0].deltaR = 0.3
-#applyPostfix( process, 'pfIsolatedMuons'      , postfix ).combinedIsolationCut = 0.2
+applyPostfix( process, 'pfSelectedMuons', postfix ).cut = 'abs(eta)<2.5 && pt>10.'
 
-applyPostfix( process, 'patMuons', postfix ).usePV      = True
+applyPostfix( process, 'pfIsolatedMuons', postfix ).doDeltaBetaCorrection = True
+applyPostfix( process, 'pfIsolatedMuons', postfix ).deltaBetaFactor = -0.5
+applyPostfix( process, 'pfIsolatedMuons', postfix ).isolationCut = 0.20
+
 applyPostfix( process, 'patMuons', postfix ).embedTrack = True
-applyPostfix( process, 'selectedPatMuons', postfix ).cut = muonCutPF
+applyPostfix( process, 'patMuons', postfix ).usePV      = False
+applyPostfix( process, 'selectedPatMuons', postfix ).cut = '''abs(eta)<2.4 && pt>20. &&
+   (chargedHadronIso+max(0.,neutralHadronIso+photonIso-0.50*puChargedHadronIso))/pt < 0.20 &&
+   (isPFMuon && (isGlobalMuon || isTrackerMuon) )'''
 
 
 # electron configuration
-applyPostfix( process, 'pfElectronsFromVertex'    , postfix ).vertices = cms.InputTag( 'goodOfflinePrimaryVertices' )
-applyPostfix( process, 'pfElectronsFromVertex'    , postfix ).d0Cut    = 0.04
-#applyPostfix( process, 'isoValElectronWithCharged', postfix ).deposits[0].deltaR = 0.3
-#applyPostfix( process, 'isoValElectronWithNeutral', postfix ).deposits[0].deltaR = 0.3
-#applyPostfix( process, 'isoValElectronWithPhotons', postfix ).deposits[0].deltaR = 0.3
-#applyPostfix( process, 'pfIsolatedElectrons'    , postfix ).combinedIsolationCut = 0.17
+applyPostfix( process, 'pfElectronsFromVertex', postfix ).vertices = (
+      cms.InputTag( 'goodOfflinePrimaryVertices' ) )
+applyPostfix( process, 'pfElectronsFromVertex', postfix ).d0Cut    = 0.04
+process.load('EGamma.EGammaAnalysisTools.electronIdMVAProducer_cfi')
+process.load('EGamma.EGammaAnalysisTools.electronIsolatorFromEffectiveArea_cfi')
+process.pfIdentifiedElectrons = cms.EDFilter("ElectronIDPFCandidateSelector", 
+      recoGsfElectrons = cms.InputTag("gsfElectrons"),
+      electronIdMap = cms.InputTag("mvaTrigV0"),
+      electronIdCut = cms.double(0.0),
+      src = cms.InputTag('pfElectronsFromVertex'+postfix))
+process.elPFIsoValueEA03.pfElectrons = cms.InputTag( 'pfSelectedElectrons'+postfix, '', '')
 
-applyPostfix( process, 'patElectrons', postfix ).usePV      = False
-applyPostfix( process, 'patElectrons', postfix ).embedTrack = True
-applyPostfix( process, 'selectedPatElectrons', postfix ).cut = electronCutPF
-applyPostfix( process, 'patElectrons', postfix ).electronIDSources = electronIDSources = cms.PSet( eidTight            = cms.InputTag( 'eidTight' )
-                                                                                                 , eidLoose            = cms.InputTag( 'eidLoose' )
-                                                                                                 , eidRobustTight      = cms.InputTag( 'eidRobustTight' )
-                                                                                                 , eidRobustHighEnergy = cms.InputTag( 'eidRobustHighEnergy' )
-                                                                                                 , eidRobustLoose      = cms.InputTag( 'eidRobustLoose' )
-                                                                                                 , eidVeryLooseMC      = cms.InputTag( 'eidVeryLooseMC' )
-                                                                                                 , eidLooseMC          = cms.InputTag( 'eidLooseMC' )
-                                                                                                 , eidMediumMC         = cms.InputTag( 'eidMediumMC' )
-                                                                                                 , eidTightMC          = cms.InputTag( 'eidTightMC' )
-                                                                                                 , eidSuperTightMC     = cms.InputTag( 'eidSuperTightMC' )
-                                                                                                 , eidHyperTight1MC    = cms.InputTag( 'eidHyperTight1MC' )
-                                                                                                 )
+applyPostfix( process, 'pfSelectedElectrons', postfix ).src = 'pfIdentifiedElectrons'
+applyPostfix( process, 'pfSelectedElectrons', postfix ).cut = '''abs(eta)<2.5 && pt>20. &&
+   gsfTrackRef.isNonnull && gsfTrackRef.trackerExpectedHitsInner.numberOfLostHits<2'''
 
+applyPostfix( process, 'pfIsolatedElectrons', postfix ).isolationValueMapsCharged = (
+      cms.VInputTag(cms.InputTag( 'elPFIsoValueCharged03PFId'+postfix )) )
+applyPostfix( process, 'pfIsolatedElectrons', postfix ).isolationValueMapsNeutral = (
+      cms.VInputTag(cms.InputTag( 'elPFIsoValueNeutral03PFId'+postfix ),
+      cms.InputTag( 'elPFIsoValueGamma03PFId'+postfix )) )
+applyPostfix( process, 'pfIsolatedElectrons', postfix ).deltaBetaIsolationValueMap = (
+   'elPFIsoValueEA03' ) # new Effective Area edm::ValueMap
+applyPostfix( process, 'pfIsolatedElectrons', postfix ).doDeltaBetaCorrection = (
+   True ) # not really a 'deltaBeta' correction, but it serves
+applyPostfix( process, 'pfIsolatedElectrons', postfix ).deltaBetaFactor = -1.0
+applyPostfix( process, 'pfIsolatedElectrons', postfix ).isolationCut = 0.15
+
+getattr( process, 'patPF2PATSequence'+postfix).replace(
+      getattr( process, 'pfSelectedElectrons'+postfix ),
+   process.mvaTrigV0 + process.pfIdentifiedElectrons +
+   getattr( process, 'pfSelectedElectrons'+postfix ) + process.elPFIsoValueEA03 )
+
+applyPostfix( process, 'patElectrons', postfix ).isolationValues.pfPhotons = (
+   'elPFIsoValueGamma03PFId'+postfix )
+applyPostfix( process, 'patElectrons', postfix ).isolationValues.pfNeutralHadrons = (
+   'elPFIsoValueNeutral03PFId'+postfix )
+applyPostfix( process, 'patElectrons', postfix ).isolationValues.pfChargedHadrons = (
+   'elPFIsoValueCharged03PFId'+postfix )
+applyPostfix( process, 'patElectrons', postfix ).isolationValues.pfChargedAll = (
+   'elPFIsoValueChargedAll03PFId'+postfix )
+applyPostfix( process, 'patElectrons', postfix ).isolationValues.pfPUChargedHadrons = (
+   'elPFIsoValuePU03PFId'+postfix )
+applyPostfix( process, 'patElectrons', postfix ).isolationValues.user = (
+   cms.VInputTag(cms.InputTag("elPFIsoValueEA03")) )
+applyPostfix( process, 'patElectrons', postfix ).electronIDSources = (
+   cms.PSet( mvaTrigV0 = cms.InputTag("mvaTrigV0")) )
+
+applyPostfix( process, 'selectedPatElectrons', postfix ).cut = '''abs(eta)<2.5 && pt>20. && (chargedHadronIso+max(0.,neutralHadronIso+photonIso-1.0*userIsolation("User1Iso")))/pt < 0.15 && electronID("mvaTrigV0") > 0.5 && passConversionVeto'''
 
 # PAT requires 2 leptons
 process.countPatLeptons.minNumber = 2
@@ -331,22 +378,10 @@ applyPostfix( process, 'patJetCorrFactors', postfix ).rho = cms.InputTag( 'kt6PF
 process.out.outputCommands.append( 'keep double_*' + postfix + '*_*_' + process.name_() )
 
 
-# ElectronID
-process.load( "RecoEgamma.ElectronIdentification.cutsInCategoriesElectronIdentificationV06_cfi" )
-process.eidCiCSequence = cms.Sequence(
-  process.eidVeryLooseMC
-+ process.eidLooseMC
-+ process.eidMediumMC
-+ process.eidTightMC
-+ process.eidSuperTightMC
-+ process.eidHyperTight1MC
-)
-
-
 # pair selection
 process.eePair = cms.EDProducer("CandViewShallowCloneCombiner",
     decay = cms.string("selectedPatElectrons"+postfix+"@- selectedPatElectrons"+postfix+"@+"),
-    cut = cms.string("mass > 12"),
+    cut = cms.string("mass > 20"),
 )
 process.eePairFilter = cms.EDFilter("CandViewCountFilter",
     src = cms.InputTag("eePair"),
@@ -356,7 +391,7 @@ process.step1ee = cms.Sequence(process.eePair*process.eePairFilter)
 
 process.emuPair = cms.EDProducer("CandViewShallowCloneCombiner",
     decay = cms.string("selectedPatElectrons"+postfix+"@- selectedPatMuons"+postfix+"@+"),
-    cut = cms.string("mass > 12"),
+    cut = cms.string("mass > 20"),
 )
 process.emuPairFilter = cms.EDFilter("CandViewCountFilter",
     src = cms.InputTag("emuPair"),
@@ -366,7 +401,7 @@ process.step1emu = cms.Sequence(process.emuPair*process.emuPairFilter)
 
 process.mumuPair = cms.EDProducer("CandViewShallowCloneCombiner",
     decay = cms.string("selectedPatMuons"+postfix+"@- selectedPatMuons"+postfix+"@+"),
-    cut = cms.string("mass > 12"),
+    cut = cms.string("mass > 20"),
 )
 process.mumuPairFilter = cms.EDFilter("CandViewCountFilter",
     src = cms.InputTag("mumuPair"),
@@ -406,7 +441,7 @@ process.step3 = cms.EDFilter("CandViewCountFilter",
 # MET filter
 process.highMET = cms.EDFilter("CandViewSelector",
     src = cms.InputTag("patMETs"+postfix),
-    cut = cms.string("pt>30")
+    cut = cms.string("pt>40")
 )
 
 process.highMETFilter = cms.EDFilter("CandViewCountFilter",
@@ -414,22 +449,22 @@ process.highMETFilter = cms.EDFilter("CandViewCountFilter",
     minNumber = cms.uint32(1)
 )
 
-process.highMETemu = cms.EDFilter("CandViewSelector",
-    src = cms.InputTag("patMETs"+postfix),
-    cut = cms.string("pt>20")
-)
+#process.highMETemu = cms.EDFilter("CandViewSelector",
+#    src = cms.InputTag("patMETs"+postfix),
+#    cut = cms.string("pt>20")
+#)
 
-process.highMETFilteremu = cms.EDFilter("CandViewCountFilter",
-    src = cms.InputTag("highMETemu"),
-    minNumber = cms.uint32(1)
-)
+#process.highMETFilteremu = cms.EDFilter("CandViewCountFilter",
+#    src = cms.InputTag("highMETemu"),
+#    minNumber = cms.uint32(1)
+#)
 
 
 process.step4 = cms.Sequence(process.highMET*process.highMETFilter)
-process.step4emu = cms.Sequence(process.highMETemu*process.highMETFilteremu)
+process.step4emu = cms.Sequence() #process.highMETemu*process.highMETFilteremu)
 
 # trigger filter
-trigger_paths = ["HLT_Mu17_Mu8_v"," HLT_Ele17_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_Ele8_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_v"," HLT_Mu17_Ele8_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_v7","HLT_Mu8_Ele17_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_v7"]
+#trigger_paths = ["HLT_Mu17_Mu8_v","HLT_Ele17_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_Ele8_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_v","HLT_Mu17_Ele8_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_v*","HLT_Mu8_Ele17_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_v*"]
 trigger_pattern = [path+"*" for path in trigger_paths]
 
 from HLTrigger.HLTfilters.hltHighLevel_cfi import *
@@ -461,15 +496,10 @@ if signalMC or TauMC or TopNonDilMC:
     process.p_ee += process.makeGenEvt
     process.p_emu += process.makeGenEvt
     process.p_mumu += process.makeGenEvt
-    #process.p_ee += process.ttDilFilter
-    #process.p_emu += process.ttDilFilter
-    #process.p_mumu += process.ttDilFilter
+    process.p_ee += process.ttDilFilter
+    process.p_emu += process.ttDilFilter
+    process.p_mumu += process.ttDilFilter
 
-if runOnMC :
-    process.p_ee += process.totalKinematicsFilter
-    process.p_emu += process.totalKinematicsFilter
-    process.p_mumu += process.totalKinematicsFilter
-    
 process.p_ee += process.eventCleaning
 process.p_emu += process.eventCleaning
 process.p_mumu += process.eventCleaning
@@ -478,14 +508,13 @@ process.p_ee += process.vertexing
 process.p_emu += process.vertexing
 process.p_mumu += process.vertexing
 
-process.p_ee += process.eidCiCSequence
-process.p_emu += process.eidCiCSequence
-process.p_mumu += process.eidCiCSequence # this is probably still needed to make pat not crash
-
-
 process.p_ee += getattr( process, 'patPF2PATSequence' + postfix )
 process.p_emu += getattr( process, 'patPF2PATSequence' + postfix )
 process.p_mumu += getattr( process, 'patPF2PATSequence' + postfix )
+
+process.p_ee += process.pfIdentifiedElectrons
+process.p_emu += process.pfIdentifiedElectrons
+process.p_mumu += process.pfIdentifiedElectrons
 
 process.p_ee += process.step1ee
 process.p_emu += process.step1emu
